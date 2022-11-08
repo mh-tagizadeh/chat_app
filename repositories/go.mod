9b7e99783ed23d3963dbf97e8bd03a5c583565e3
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2022-11-09 00:35:47 +0330

    create user repository

---
+++ repositories/go.mod
@@ -0,0 +1,13 @@
+module github.com/mh-tagizadeh/chat_app/repositories
+
+go 1.18
+
+require (
+	github.com/mh-tagizadeh/chat_app/models v0.0.0-20221108210448-be41698ba933
+	gorm.io/gorm v1.24.1
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+)
---
+++ repositories/userRepository.go
@@ -0,0 +1,25 @@
+package repositories
+
+import (
+	"gorm.io/gorm"
+	"github.com/mh-tagizadeh/chat_app/models"
+)
+
+
+type IUserRepository interface {
+	Migrateable
+}
+
+
+// UserRepository its data access layer of user.
+type UserRepository struct {
+	Database *gorm.DB
+}
+
+// Migrate generate tables from the databases 
+// @return error 
+func (repository *UserRepository) Migrate() (err error) {
+	err := repository.Database.AutoMigrate(models.User{})
+	return 
+}
+
