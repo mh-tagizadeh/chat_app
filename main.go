6f5562e2a32d5dfc12562d08db218ddc73a13c65
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2022-11-12 18:46:32 +0330

    update main.go

--- main.go
+++ main.go
@@ -2,42 +2,26 @@ package main
 
 
 import (
-	"gorm.io/gorm"
-    "gorm.io/driver/postgres"
+    "github.com/gin-gonic/gin"
 
-
-	"github.com/mh-tagizadeh/chat_app/repositories"
+	"github.com/mh-tagizadeh/chat_app/database"
+	"os"
 )
 
-// Options has the options for initating the chat_app
-type Options struct {
-	Migrate bool
-	DB *gorm.DB
-}
-
-// New initializer for chat_app
-// If migration is true, it generate all tables in the database if they don't exist.
-func InitializeDatabase(opts Options) (err error) {
-	userRepository := &repositories.UserRepository{Database: opts.DB}
-
-
-	if opts.Migrate {
-		err = repositories.Migrates(userRepository)
-		if err != nil {
-			return err
-		}
-	}
-
-	return 
-}
-
-
 func main(){
-	dsn := "host=localhost user=admin password=laravel dbname=chat_app_db port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	db, _ := gorm.Open(postgres.Open(dsn), &gorm.Config{})// hello
 
-	InitializeDatabase(Options{
+
+	// Connect with database
+	// @parameter connection_string => Database details for connect postgres database
+	database.Connect(os.Getenv("DATABASE_CONNECTION"))
+
+	// Migrate tables 
+	database.Migrates(Options{
 		Migrate: true,
-		DB: db,
+		DB: database.Connection,
 	})
+
+    router := gin.Default()
+
+	router.Run("localhost:8080")
 }
