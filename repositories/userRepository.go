a921373bb1e0e5d50e9d276c572d09571cbcc399
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2022-11-13 03:28:38 +0330

    complete userRepository interface

--- repositories/userRepository.go
+++ repositories/userRepository.go
@@ -8,6 +8,26 @@ import (
 
 type IUserRepository interface {
 	Migratable
+
+	// single fetch options
+
+	GetUserByID(ID uint) (user models.User, err error)
+
+
+	// multipile fetch options
+
+	GeUsers(ID uint) (user models.User, err error)
+
+	// ID fetch options
+
+	GetUserIDs(pagination scopes.GormPager) (userIDs []uint, totalCount int64, err error)
+
+	// FirstOrCreate & Updates & Delete
+
+	FirstOrCreate(user *models.User) (err error)
+	Updates(user *models.User, updates map[string]interface{}) (err error)
+	Delete(user *models.User) (err error)
+
 }
 
 
