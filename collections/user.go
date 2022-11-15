73cb7dda4b101325634760876b81f7971c81a75e
mh-tagizadeh <mohammadhusseinbvb09@gmail.com>
2022-11-16 02:29:26 +0330

    update user.go collections

--- collections/user.go
+++ collections/user.go
@@ -4,26 +4,26 @@ import (
 	"github.com/mh-tagizadeh/chat_app/models"
 )
 
-// Role provides methods for you to manage array data more easily.
-type Role []models.Role
+// User provides methods for you to manage array data more easily.
+type User []models.User
 
-// Origin convert the collection to role array.
-// @return []models.Role
-func (u Role) Origin() []models.Role {
-	return []models.Role(u)
+// Origin convert the collection to user array.
+// @return []models.User
+func (u User) Origin() []models.User {
+	return []models.User(u)
 }
 
 // Len returns the number of elements of the array.
 // @return int64
-func (u Role) Len() (length int64) {
+func (u User) Len() (length int64) {
 	return int64(len(u))
 }
 
-// IDs returns an array of the role array's ids.
+// IDs returns an array of the user array's ids.
 // @return []uint
-func (u Role) IDs() (IDs []uint) {
-	for _, role := range u {
-		IDs = append(IDs, role.ID)
+func (u User) IDs() (IDs []uint) {
+	for _, user := range u {
+		IDs = append(IDs, user.ID)
 	}
 	return IDs
 }
